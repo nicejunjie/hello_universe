ea9f6753d4cf24bb63b02e6c72b499954ee940b4
Junjie Li <junjieli@login3.frontera.tacc.utexas.edu>
2021-06-18 16:52:51 -0500

    test

--- ompt/ongpu.c
+++ ompt/ongpu.c
@@ -3,19 +3,21 @@
 
 int main()
 {
+
   int runningOnGPU = 0;
   /* Test if GPU is available using OpenMP4.5 */
-#pragma omp target map(from:runningOnGPU)
+#pragma omp target map(tofrom:runningOnGPU)
   {
     if (omp_is_initial_device() == 0)
       runningOnGPU = 1;
   }
   /* If still running on CPU, GPU must not be available */
   if (runningOnGPU)
-    printf("### Able to use the GPU! ### \n");
+    printf("### Yes, able to use the GPU! ### \n");
   else
-    printf("### Unable to use the GPU, using CPU! ###\n");
+    printf("### No, unable to use the GPU, using CPU! ###\n");
 
   return 0;
 }
+
 
